b914b1ce29302cbca2b80dba0939cfdebe990e28
Mario-78 <mariojsws1@gmail.com>
2021-11-08 23:21:59 -0300

    Possibilidades de EXPORT, EXPORT DEFAULT e IMPORT

---
+++ src/componentes/Multiplos.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// export const BoaTarde = props => <h1>Boa Tarde {props.name}!</h1>
+const BoaTarde = props => <h1>Boa Tarde {props.name}!</h1>
+
+// export const BoaNoite = props => <h1>Boa Noite {props.name}!</h1>
+const BoaNoite = props => <h1>Boa Noite {props.name}!</h1>
+
+export {BoaTarde, BoaNoite}
+
+export default {
+    BoaTarde,
+    BoaNoite
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom'
 
 import Primeiro from './componentes/Primeiro'
 import BomDia from './componentes/BomDia'
+// import { BoaTarde, BoaNoite } from './componentes/Multiplos'
+import Mult from './componentes/Multiplos'
 
-ReactDOM.render(<BomDia name="Mário"/>, document.getElementById('root'));
+// ReactDOM.render(
+//     <div>
+//         <BoaTarde name='Bia'/>
+//         <BoaNoite name="Mário"/>,
+//     </div>,
+//     document.getElementById('root'));
+
+    ReactDOM.render(
+    <div>
+        <Mult.BoaTarde name='Bia'/>
+        <Mult.BoaNoite name="Mário"/>,
+    </div>,
+    document.getElementById('root'));
+
+
+
 // ReactDOM.render(<BomDia />, document.getElementById('root'));
 
 
