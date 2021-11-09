ab244f5d6696a467912119d033a886fb36ccfde1
Mario-78 <mariojsws1@gmail.com>
2021-11-08 22:19:13 -0300

    Primeiro componente

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1> Hello, world!!! </h1>, document.getElementById('root'));
+import Primeiro from './componentes/primeiro'
+
+ReactDOM.render(<Primeiro />, document.getElementById('root'));
 
 
