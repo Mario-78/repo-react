08dbb75e63e5a4b126217bc88cbe749a4505659a
Mario-78 <mariojsws1@gmail.com>
2021-11-09 13:41:30 -0300

    State e props

---
+++ src/componentes/Saudacao.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+
+export default class Saudacao extends Component{
+
+    state = {
+        // tipo:'Fala',
+        // name: 'Mário',
+        tipo:this.props.tipo,
+        name: this.props.name
+    }
+
+    setTipo(e){
+        // console.log(e.target.value)
+        // this.props.tipo = e.target.value
+        // let i = 1
+        // setInterval(() => {
+        //     this.setState({tipo: ++i})
+        // }, 1000)
+        this.setState({ tipo: e.target.value})
+        
+    }
+
+    setName(e){
+        this.setState({name: e.target.value})
+    }
+    render(){
+        // const{tipo, name} = this.props
+        const{tipo, name} = this.state
+        return(
+            <div>
+                <h1>{tipo} {name}!</h1>
+                <hr />
+                <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)} />
+                <input type="text" placeholder="Nome..." value={name} onChange={e => this.setName(e)} />
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Primeiro from './componentes/Primeiro'
 import BomDia from './componentes/BomDia'
 // import { BoaTarde, BoaNoite } from './componentes/Multiplos'
 import Mult from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
 
 // ReactDOM.render(
 //     <div>
@@ -15,8 +16,9 @@ import Mult from './componentes/Multiplos'
 
     ReactDOM.render(
     <div>
+        <Saudacao tipo='Bom dia' name='Mário'/>
         <Mult.BoaTarde name='Bia'/>
-        <Mult.BoaNoite name="Mário"/>,
+        <Mult.BoaNoite name="Mário"/>
     </div>,
     document.getElementById('root'));
 
